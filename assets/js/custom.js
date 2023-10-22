b172d66e88cea7cfb55c79438d8662de1476ea19
lucy <loveyou_06@naver.com>
2023-10-23 01:30:49 +0900

    Update custom.js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -9,7 +9,7 @@ var profile = {
 
         window.addEventListener('scroll', function() {
             let button = document.querySelector('.scroll-to-top');
-            if (window.scrollY > 0) { 
+            if (window.scrollY > 10) { 
                 button.style.display = 'block';
             } else {
                 button.style.display = 'none';
